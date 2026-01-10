f596f0374e717078136d603d7bd4170d96db53ae
TJ <tjxjnoobie@gmail.com>
2026-01-10 01:12:23 -0800

    Added: `Gemini3ImageClient` class to handle client setup for Gemini 3 image processing modes

---
+++ gemini-api/src/main/java/org/tavall/gemini/clients/Gemini3ImageClient.java
@@ -0,0 +1,43 @@
+package org.tavall.gemini.clients;
+
+
+import com.google.genai.Client;
+import com.google.genai.types.HttpOptions;
+import org.tavall.gemini.enums.GeminiAPIVersion;
+import org.tavall.gemini.enums.GeminiModel;
+import org.tavall.gemini.generate.Schemas;
+
+public class Gemini3ImageClient extends Client.Builder{
+
+    private Client client;
+
+    private final double TEMPERATURE = 0.1;
+    private GeminiModel geminiModel;
+    private GeminiAPIVersion geminiAPIVersion;
+    private Schemas schemas;
+    public Gemini3ImageClient() {
+
+        buildGemini3ImageClient();
+    }
+
+    public Gemini3ImageClient(Client client) {
+        this.client = client;
+    }
+
+
+    public void buildGemini3ImageClient() {
+
+        this.client = Client.builder().apiKey
+                        (System.getenv("GEMINI_API_KEY"))
+                .httpOptions(HttpOptions.builder().
+                        apiVersion(String.valueOf(GeminiAPIVersion.V1))
+                        .build())
+                .build();
+    }
+
+    public Client getClient() {
+        return this.client;
+    }
+
+
+}
