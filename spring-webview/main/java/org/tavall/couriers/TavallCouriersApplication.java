0988d2f210b8963fa18b18990dc4de4d60777354
TJ <tjxjnoobie@gmail.com>
2026-01-13 00:36:07 -0800

    Added: Basic Spring Boot structure with static resources and test scaffolding

--- spring-webview/main/java/org/tavall/couriers/TavallCouriersApplication.java
+++ spring-webview/main/java/org/tavall/couriers/TavallCouriersApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TavallCouriersApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(TavallCouriersApplication.class, args);
     }
 
---
+++ spring-webview/test/java/org/tavall/couriers/TavallCouriersApplicationTests.java
@@ -0,0 +1,14 @@
+package org.tavall.couriers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavallCouriersApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
