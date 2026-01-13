46c3368ac16e454b47ed902ccfbf12b0b44c4a4f
TJ <tjxjnoobie@gmail.com>
2026-01-13 00:37:23 -0800

    Added: `AppEndpoint` interface to define structure for API endpoint and method operations.

---
+++ internal-courier-api/src/main/java/org/tavall/couriers/api/web/endpoints/interfaces/AppEndpoint.java
@@ -0,0 +1,16 @@
+/*
+ * TJVD License (TJ Valentine’s Discretionary License) — Version 1.0 (2025)
+ *
+ * Copyright (c) 2025 Taheesh Valentine
+ *
+ * This source code is protected under the TJVD License.
+ * SEE LICENSE.TXT
+ */
+
+package org.tavall.couriers.api.web.endpoints.interfaces;
+
+public interface AppEndpoint {
+
+    String endpoint();
+    String method(); // POST, GET
+}
